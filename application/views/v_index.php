cf7afe5721a589b1c2099709cb3fdfe3a674e33c
Dan <45204353+ramdan2478@users.noreply.github.com>
2019-11-02 03:42:02 +0700

    Add bulma CSS

--- application/views/v_index.php
+++ application/views/v_index.php
@@ -3,14 +3,25 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title></title>
-	<link rel="stylesheet" href="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Index</title>
 	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url('assets/favicon/apple-touch-icon.png');?>">
 	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo base_url('assets/favicon/favicon-32x32.png');?>">
 	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url('assets/favicon/favicon-16x16.png');?>">
 	<link rel="manifest" href="<?php echo base_url('assets/favicon/site.webmanifest');?>">
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/bulma.min.css');?>">
+	<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 </head>
 <body>
-	<h1>Hello world</h1>
+	<section class="section">
+    <div class="container">
+      <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        <strong>Bulma</strong> CSS with Favicon! :)
+      </p>
+    </div>
+  </section>
 </body>
 </html>
